517cd2de9f3c34d2e8e611c4371ed289bbe2dbf3
speters-github <umpet347@myumanitoba.ca>
2021-09-07 15:13:22 -0500

    Simple recursive factorial example.

---
+++ docs/language_bootcamp/javascript/Factorial.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function factorial(num){
+    if (arguments.length !== 1 || !Number.isInteger(num) || num < 0){
+        throw new Error("Invalid use of factorial() function.  factorial() " +
+            "takes 1 argument as a parameter, which is a positive integer.");
+    }
+
+    let returnValue = 1; // value to be returned
+
+    if (num > 1){
+        returnValue = num * factorial(num - 1);
+    }
+
+    return returnValue;
+}
+
+function main(){
+    let num = 5; // number to calculate factorial on
+    let answer = factorial(num); // factorial result / answer
+
+    // output to the console the number and the answer.
+    console.log("The factorial of \"%d\" is \"%d\".", num, answer);
+}
+
+main(); // run the main() function
